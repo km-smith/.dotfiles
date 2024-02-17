2d2a302e6d3242bc1a1f8d3f3cf8ebb600a9d2af
Kyle Smith <ksmith28@apple.com>
2024-02-17 12:29:46 -0800

    Update install script

--- install.sh
+++ install.sh
@@ -5,6 +5,19 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 
 brew install stow
+brew install zsh-syntax-highlighting
+brew install n
+brew install gh
+
+# nvim
+brew install neovim
+brew install tree-sitter
+brew install ripgrep
+brew install lazygit
+brew install gdu
+brew install bottom
+
+n lts
 
 stow zsh
 stow idea
