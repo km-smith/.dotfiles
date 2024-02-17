922c949d13eef8cf9caede675ad546832eca1882
Kyle Smith <ksmith28@apple.com>
2024-02-17 12:33:59 -0800

    Cleanup comments that are obvious

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
-# https://ohmyz.sh/#install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# https://brew.sh/
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 
 brew install stow
